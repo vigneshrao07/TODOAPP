a206e514c55f1e6ce223ff8f40f6b322663506bf
vigneshrao07 <162863603+vigneshrao07@users.noreply.github.com>
2025-05-06 23:56:06 +0100

    updated code

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express=require("express");
+const { createTodo } = require("./types");
 const app=express();
 
 app.use(express.json());
 
 app.post("/todo", function(req, res){
-
+    const createPayload=req.body;
+    const parsedPayload=createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "you sent the wrong inputs",
+        })
+        return;
+    }
 })
 app.get("/todos", function(req, res){
+    
 
 })
 app.put("/completed", function(req, res){
-    
+    const updatepayload=req.body;
+    const parsedpayload=updateTodo.safeParse(updatepayload);
+    if(!parsedpayload.success){
+        res.status(411).json({
+            msg: "you have sent wrong inputs"
+        })
+        return;
+    }
+
 })
