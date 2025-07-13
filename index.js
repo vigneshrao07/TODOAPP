c07223c8aa7f60a9dc993d54a8b61d0d957a8944
vigneshrao07 <162863603+vigneshrao07@users.noreply.github.com>
2025-07-13 21:01:23 +0100

    commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
 const { title } = require("process");
+const cors=require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/todo", async (req, res) => {
   const createPayload = req.body;
